2e670e7433a4e1875da0c8cba53853b496251b7d
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-27 17:21:31 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if array.length == 0
+    "Empty"
+  else
+  i = 0
+    while (i < array.length)
+    number = array[i]
+    yield(number)
+    i += 1 
+    end
+    array
+  end
 end
+
+array = [1, 2, 3, 4]
+
+my_each(array) { |number| number }
+
